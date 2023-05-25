5131918253e4296c13e11a96fe2962804e8505ed
Micaela Alejandra Veiga <micaela.a.veiga@gmail.com>
2023-05-25 02:10:55 -0300

    Total cambia en tiempo real.

--- js/js.js
+++ js/js.js
@@ -8,18 +8,21 @@ var cantidad = 0;
 
 const TICKET = 200;
 
+const FORM_CATEGORY = document.getElementById("form-category");
+const FORM_QUANTITY = document.getElementById("form-quantity");
+
 function cuenta(cantidad_, descuento_) {
     total.innerHTML = (TICKET * cantidad_) / 100 * descuento_;
 }
 
-document.getElementById("form-category").addEventListener("change", function() {
+FORM_CATEGORY.addEventListener("change", function() {
     if (this.value == "estudiante") descuento = 80;
     else if (this.value == "trainee") descuento = 50;
     else if (this.value == "junior") descuento = 15;
     cuenta(cantidad, descuento);
 });
 
-document.getElementById("form-quantity").addEventListener("change", function() {
+FORM_QUANTITY.onkeyup = function() {
     cantidad = this.value;
     cuenta(cantidad, descuento);
-});
+};
